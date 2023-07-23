75b786e8c2825d8a71afe5f15b8e5e62ee57af14
Tony Nguyen <tbnktd@umsystem.edu>
2023-07-22 23:34:45 -0500

    first commit

---
+++ ExtraCredit/ExtraCredit/Source1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+/* Develop a spell - checker using data structure.The program should take a
+word as inputand check if it is spelled correctly.If not, it should suggest
+the closest matching words. */
+
+
+
+
+
+int main(){
+
+
+
+
+	return 0;
+}
